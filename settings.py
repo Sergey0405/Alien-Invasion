0a3f35c46bc23571e786319c04f7ea12f868451a
Sergey <sergeymashchenko21@gmail.com>
2022-02-03 18:41:00 +0300

    V3. The ship is moving faster and don't leave the limits of the screen.

--- settings.py
+++ settings.py
@@ -6,3 +6,5 @@ class Settings():
         self.screen_width = 700
         self.screen_heigth = 600
         self.bg_color = (230, 230, 230)
+        # Настройки корабля
+        self.ship_speed_factor = 1.5
